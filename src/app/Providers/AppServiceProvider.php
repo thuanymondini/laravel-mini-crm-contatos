ee13f98e5be3c30cdee3e8e7d015d3cddfc9c8d2
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-05-17 16:16:17 -0300

    feat: adicionando binding do evento

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Domain\Contact\Events\ContactScoreProcessed;
+use App\Infrastructure\Contact\Listeners\LogContactScoreListener;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(
+            ContactScoreProcessed::class,
+            LogContactScoreListener::class
+        );
     }
 }
