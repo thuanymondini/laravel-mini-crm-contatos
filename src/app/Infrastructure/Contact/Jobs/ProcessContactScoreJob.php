0243758f8f9a687ff8254461b178bdd9642099ea
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-05-17 16:01:13 -0300

    feat: adicionando job para calculo de score

---
+++ src/app/Infrastructure/Contact/Jobs/ProcessContactScoreJob.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Infrastructure\Contact\Jobs;
+
+use App\Application\Contact\UseCases\ProcessContactScoreUseCase;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ProcessContactScoreJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(public readonly int $contactId) {}
+
+    public function handle(ProcessContactScoreUseCase $useCase): void
+    {
+        sleep(2);
+        $useCase->execute($this->contactId);
+    }
+
+    public function handleForTesting(ProcessContactScoreUseCase $useCase): void
+    {
+        $useCase->execute($this->contactId);
+    }
+}
