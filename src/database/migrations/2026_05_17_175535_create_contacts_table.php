3b911ab66be9ed1fbfdf6bbcaf04b2457141a620
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-05-17 15:20:52 -0300

    feat: adicionando migracao para tabela de contacts

---
+++ src/database/migrations/2026_05_17_175535_create_contacts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->integer('score')->default(0);
+            $table->string('status')->default('pending');
+            $table->timestamp('processed_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
