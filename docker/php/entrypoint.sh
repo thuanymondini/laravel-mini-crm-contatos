df2fad7af6af74e5ce6e150b544bcb3485090a7f
thuany.mondini <thuanydesouza26@gmail.com>
2026-05-18 15:28:14 -0300

    Fix: alterações para garantir a estabilidade e a orquestração da ordem de subida dos containers

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache
-
+chown -R www-data:www-data /var/www
+chmod -R 775 /var/www
 # Instala dependências se vendor não existir
 if [ ! -f "/var/www/vendor/autoload.php" ]; then
     composer install --no-interaction --optimize-autoloader
@@ -12,6 +11,5 @@ php artisan optimize
 if [ "$#" -gt 0 ]; then
     exec "$@"
 else
-    php artisan migrate
     exec php-fpm
 fi
