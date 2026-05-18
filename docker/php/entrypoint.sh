2cf2998452dcc8f1ba41d58673d0f6561c9940ff
thuanymondini <thuanydesouza26@gmail.com>
2026-05-18 13:25:56 -0300

    fix: correções para subir o ambiente corretamente

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -2,9 +2,16 @@
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
+# Instala dependências se vendor não existir
+if [ ! -f "/var/www/vendor/autoload.php" ]; then
+    composer install --no-interaction --optimize-autoloader
+fi
+
+php artisan optimize
 
 if [ "$#" -gt 0 ]; then
     exec "$@"
 else
+    php artisan migrate
     exec php-fpm
 fi
