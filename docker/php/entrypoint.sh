54181022260417c410d7d135d52ca11a9a9d8a02
ThuanyDeSouza <thuanydesouza26@gmail.com>
2026-05-17 22:13:52 -0300

    chore: configurando infraestrutura para suportar o processamento de filas automatico quando os containeres sobem, e tambem rodar o reverb automaticamente

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
-exec php-fpm
+
+
+if [ "$#" -gt 0 ]; then
+    exec "$@"
+else
+    exec php-fpm
+fi
